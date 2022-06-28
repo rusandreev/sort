d77d2ab141e11ade484ab638d7a7156555b223f6
Ruslan Andreev <rus.andreev@gmail.com>
2022-06-28 17:27:19 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-class Sorter {
-    constructor(public collection: number[]) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-    sort(): void {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 100, -100, 1]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 100, -100]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
